b0290892b8d8c46c79bb346ec839dd0053c0ae8d
Michael Spengler <michael.spengler@sap.com>
2019-01-22 19:27:30 +0100

    first commit

---
+++ src/telegram-consumer.ts
@@ -0,0 +1,28 @@
+import { TelegramInteractionBuilder, DefaultResponseProvider, AdvancedResponseProvider }
+    from "telegram-interaction-builder"
+
+export class TelegramConsumer {
+    private interactionBuilder: TelegramInteractionBuilder
+    private advancedResponseProvider: AdvancedResponseProvider
+
+    public constructor() {
+
+        // remember to include its name in .gitignore to keep your BOT_TOKEN secret
+        const pathToYourConfigFile: string = "../../../.env"
+
+        this.advancedResponseProvider = new AdvancedResponseProvider()
+
+        this.interactionBuilder =
+            new TelegramInteractionBuilder(pathToYourConfigFile, new DefaultResponseProvider())
+    }
+
+    public async startMyTelegramChatBot(): Promise<void> {
+        await this.advancedResponseProvider.learn("exampleMap")
+
+        this.interactionBuilder.setResponseProvider(this.advancedResponseProvider)
+        this.interactionBuilder.startListening()
+    }
+}
+
+const consumer: TelegramConsumer = new TelegramConsumer()
+consumer.startMyTelegramChatBot()
